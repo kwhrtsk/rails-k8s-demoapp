a8faf7f65d506e612f95842c0cb7ec26556101e9
kawahara.taisuke <kawahara.taisuke@gmail.com>
2018-04-11 00:06:16 +0900

    setup bootstrap4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def navbar_item(name, path, options = {})
+    options = options.merge(
+      class: "#{options[:class]} nav-link".strip,
+      wrap_tag: "li",
+      wrap_class: "nav-item",
+    )
+    active_link_to(name, path, options)
+  end
 end
--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -7,4 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'bootstrap';
+
+import './application.scss';
+
 console.log('Hello World from Webpacker');
